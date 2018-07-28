52edbdc1da9ee255c547035d9a7f4b49bb0d6940
Kyle Hammond <kyle.hammond@gmail.com>
2018-07-28 12:08:21 -0500

    class basics

--- hello-world/ts-hello/main.ts
+++ hello-world/ts-hello/main.ts
@@ -1,19 +1,32 @@
-interface Point {
-    x: number,
-    y: number
-    draw: () => void
+class Point {
+    x: number;
+    y: number;
+    
+    draw: () {
+        //...
+    }
+    
+    getDistance(another: Point){
+        //...
+    }
 }
 
-let drawPoint = (point: Point) => {
-    // ...
-}
+// interface Point {
+//     x: number,
+//     y: number
+//     draw: () => void
+// }
 
-let getDistance = (pointA: Point, pointB: Point) => {
-    // ...
-}
+// let drawPoint = (point: Point) => {
+//     // ...
+// }
 
-drawPoint(
-    {
-        x: 1,
-        y: 2
-    });
+// let getDistance = (pointA: Point, pointB: Point) => {
+//     // ...
+// }
+
+// drawPoint(
+//     {
+//         x: 1,
+//         y: 2
+//     });
