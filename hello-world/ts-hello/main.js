bece4d1c854a62522bcb602a79bc719c9f0d1fc8
Kyle Hammond <kyle.hammond@gmail.com>
2018-07-28 12:12:26 -0500

    more class basics, new keyword

--- hello-world/ts-hello/main.js
+++ hello-world/ts-hello/main.js
@@ -1,4 +1,31 @@
-var log = function (message) {
-    console.log(message);
-};
-var doLog = function (message) { return console.log(message); };
+var Point = /** @class */ (function () {
+    function Point() {
+    }
+    Point.prototype.draw = function () {
+        console.log('X: ' + this.x + ' Y: ' + this.y);
+    };
+    Point.prototype.getDistance = function (another) {
+        //...
+    };
+    return Point;
+}());
+var point = new Point();
+point.x = 1;
+point.y = 2;
+point.draw();
+// interface Point {
+//     x: number,
+//     y: number
+//     draw: () => void
+// }
+// let drawPoint = (point: Point) => {
+//     // ...
+// }
+// let getDistance = (pointA: Point, pointB: Point) => {
+//     // ...
+// }
+// drawPoint(
+//     {
+//         x: 1,
+//         y: 2
+//     });
--- hello-world/ts-hello/main.ts
+++ hello-world/ts-hello/main.ts
@@ -2,14 +2,20 @@ class Point {
     x: number;
     y: number;
     
-    draw: () {
-        //...
+    draw() {
+        console.log('X: ' + this.x + ' Y: ' + this.y)
     }
     
     getDistance(another: Point){
         //...
     }
 }
+
+let point = new Point();
+point.x = 1;
+point.y = 2;
+point.draw();
+
 
 // interface Point {
 //     x: number,
